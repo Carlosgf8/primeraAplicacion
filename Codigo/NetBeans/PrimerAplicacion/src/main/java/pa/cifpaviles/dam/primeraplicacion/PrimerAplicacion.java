959baac49925d0b9ba8ef0e0847c8ab0e192fe3d
Carlos Garcia Fernandez <carlosgf81@educastur.es>
2023-09-22 19:30:53 +0200

    Primera Version.

---
+++ Codigo/NetBeans/PrimerAplicacion/src/main/java/pa/cifpaviles/dam/primeraplicacion/PrimerAplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pa.cifpaviles.dam.primeraplicacion;
+
+/**
+ *
+ * @author carlosgf
+ */
+public class PrimerAplicacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
